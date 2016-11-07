23dea8c851bd02d140127fcb1f0c4cbe175e7a58
Joseph Burnett <josephburnett79@gmail.com>
2016-11-07 04:24:22 +0000

    Diff and basic test.

--- jd.go
+++ jd.go
@@ -2,17 +2,129 @@ package jd
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"reflect"
 )
 
-type jsonNode map[string]interface{}
-
-func (a *jsonNode) equals(b *jsonNode) bool {
-	return reflect.DeepEqual(a, b)
+type jsonNode interface {
+	diff(b jsonNode, path path) []diffElement
+	equals(b jsonNode) bool
 }
 
-func readFile(filename string) (*jsonNode, error) {
+// type jsonList []interface{}
+type jsonNumber float64
+
+func (n1 jsonNumber) equals(n jsonNode) bool {
+	n2, ok := n.(jsonNumber)
+	if !ok {
+		return false
+	}
+	if n1 != n2 {
+		return false
+	}
+	return true
+}
+
+func (n1 jsonNumber) diff(n jsonNode, path path) []diffElement {
+	d := make([]diffElement, 0)
+	if n1.equals(n) {
+		return d
+	}
+	e := diffElement{
+		path:     path.clone(),
+		oldValue: n1,
+		newValue: n,
+	}
+	return append(d, e)
+}
+
+type jsonStruct map[string]interface{}
+
+func (s1 jsonStruct) equals(n jsonNode) bool {
+	s2, ok := n.(jsonStruct)
+	if !ok {
+		return false
+	}
+	if len(s1) != len(s2) {
+		return false
+	}
+	return reflect.DeepEqual(s1, s2)
+}
+
+func newJsonNode(n interface{}) jsonNode {
+	switch t := n.(type) {
+	case map[string]interface{}:
+		return jsonStruct(t)
+	// case []interface{}:
+	// 	return jsonList(t)
+	case float64:
+		return jsonNumber(t)
+	default:
+		panic(fmt.Sprintf("Unexpected type %v", t))
+	}
+}
+
+func (s1 jsonStruct) diff(n jsonNode, path path) []diffElement {
+	d := make([]diffElement, 0)
+	s2, ok := n.(jsonStruct)
+	if !ok {
+		// Different types
+		e := diffElement{
+			path:     path.clone(),
+			oldValue: s1,
+			newValue: n,
+		}
+		return append(d, e)
+	}
+	for k1, n1 := range s1 {
+		v1 := newJsonNode(n1)
+		if n2, ok := s2[k1]; ok {
+			// Both keys are present
+			v2 := newJsonNode(n2)
+			subDiff := v1.diff(v2, append(path.clone(), k1))
+			d = append(d, subDiff...)
+		} else {
+			// S2 missing key
+			e := diffElement{
+				path:     append(path.clone(), k1),
+				oldValue: v1,
+				newValue: nil,
+			}
+			d = append(d, e)
+		}
+	}
+	for k2, n2 := range s2 {
+		v2 := newJsonNode(n2)
+		if _, ok := s1[k2]; !ok {
+			// S1 missing key
+			e := diffElement{
+				path:     append(path.clone(), k2),
+				oldValue: nil,
+				newValue: v2,
+			}
+			d = append(d, e)
+		}
+	}
+	return d
+}
+
+type pathElement interface{}
+type path []pathElement
+
+type diffElement struct {
+	path     path
+	oldValue jsonNode
+	newValue jsonNode
+}
+
+func (p1 path) clone() path {
+	p2 := make(path, len(p1), len(p1)+1)
+	copy(p2, p1)
+	return p2
+}
+
+func readFile(filename string) (jsonNode, error) {
 	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -20,11 +132,11 @@ func readFile(filename string) (*jsonNode, error) {
 	return unmarshal(bytes)
 }
 
-func unmarshal(bytes []byte) (*jsonNode, error) {
-	node := make(jsonNode)
-	err := json.Unmarshal(bytes, node)
+func unmarshal(bytes []byte) (jsonNode, error) {
+	node := make(jsonStruct)
+	err := json.Unmarshal(bytes, &node)
 	if err != nil {
 		return nil, err
 	}
-	return &node, nil
+	return node, nil
 }
--- jd_test.go
+++ jd_test.go
@@ -1,12 +1,13 @@
 package jd
 
 import (
+	"reflect"
 	"testing"
 )
 
-func testEquals(t *testing.T) {
+func TestEquals(t *testing.T) {
 	checkEqual(t, `{"a":1}`, `{"a":1}`)
-	checkEqual(t, `{"a":1}`, `{"a":1.0`)
+	checkEqual(t, `{"a":1}`, `{"a":1.0}`)
 	checkEqual(t, `{"a":[1,2]}`, `{"a":[1,2]}`)
 	checkNotEqual(t, `{"a":1}`, `{"b":1}`)
 	checkNotEqual(t, `{"a":[1,2]}`, `{"a":[2,1]}`)
@@ -49,5 +50,32 @@ func checkNotEqual(t *testing.T, a, b string) {
 	}
 	if nodeB.equals(nodeA) {
 		t.Errorf("nodeB.equals(nodeA) == true. Want false.")
+	}
+}
+
+func TestDiff(t *testing.T) {
+	checkDiff(t, `{"a":1}`, `{"a":2}`,
+		[]diffElement{
+			diffElement{
+				path:     path{"a"},
+				oldValue: jsonNumber(1.0),
+				newValue: jsonNumber(2.0),
+			},
+		})
+}
+
+func checkDiff(t *testing.T, a, b string, diff []diffElement) {
+	jsonA, err := unmarshal([]byte(a))
+	if err != nil {
+		t.Error(err.Error())
+	}
+	jsonB, err := unmarshal([]byte(b))
+	if err != nil {
+		t.Error(err.Error())
+	}
+	path := make([]pathElement, 0)
+	d := jsonA.diff(jsonB, path)
+	if !reflect.DeepEqual(d, diff) {
+		t.Errorf("!reflect.DeepEqual(d, diff). d was %v. diff was %v.", d, diff)
 	}
 }
