9332a62350aa7eebc6b0d2c7c6d8d79cd02988bf
Joseph Burnett <josephburnett79@gmail.com>
2016-11-09 20:20:22 -0800

    More Diff.Render() tests.

--- diff_test.go
+++ diff_test.go
@@ -4,11 +4,32 @@ import (
 	"testing"
 )
 
-func TestDiffSimple(t *testing.T) {
-	checkDiffRender(t, `{"a":1}`, `{"a":2}`, "@ [\"a\"]\n- 1\n+ 2\n")
+func TestDiffRender(t *testing.T) {
+	checkDiffRender(t, `{"a":1}`, `{"a":2}`,
+		`@ ["a"]`,
+		`- 1`,
+		`+ 2`)
+	checkDiffRender(t, `{"a":{"b":1}}`, `{"a":{"b":2}}`,
+		`@ ["a","b"]`,
+		`- 1`,
+		`+ 2`)
+	checkDiffRender(t, `{"a":{"b":1}}`, `{"a":{"c":2}}`,
+		`@ ["a","b"]`,
+		`- 1`,
+		`@ ["a","c"]`,
+		`+ 2`)
+	checkDiffRender(t, `{"a":{"b":1}}`, `{"c":{"b":1}}`,
+		`@ ["a"]`,
+		`- {"b":1}`,
+		`@ ["c"]`,
+		`+ {"b":1}`)
 }
 
-func checkDiffRender(t *testing.T, a, b, diff string) {
+func checkDiffRender(t *testing.T, a, b string, diffLines ...string) {
+	diff := ""
+	for _, dl := range diffLines {
+		diff += dl + "\n"
+	}
 	aJson, err := unmarshal([]byte(a))
 	if err != nil {
 		t.Errorf(err.Error())
