9b94bb0be32f436670056d63c01d88a4cc84b0bd
Joseph Burnett <josephburnett79@gmail.com>
2016-11-14 14:38:39 -0800

    Rename Test(Not)Equal to TestStruct(Not)Equal.

--- struct_test.go
+++ struct_test.go
@@ -4,14 +4,14 @@ import (
 	"testing"
 )
 
-func TestEqual(t *testing.T) {
+func TestObjectEqual(t *testing.T) {
 	checkEqual(t, `{"a":1}`, `{"a":1}`)
 	checkEqual(t, `{"a":1}`, `{"a":1.0}`)
 	checkEqual(t, `{"a":[1,2]}`, `{"a":[1,2]}`)
 	checkEqual(t, `{"a":"b"}`, `{"a":"b"}`)
 }
 
-func TestNotEqual(t *testing.T) {
+func TestObjectNotEqual(t *testing.T) {
 	checkNotEqual(t, `{"a":1}`, `{"b":1}`)
 	checkNotEqual(t, `{"a":[1,2]}`, `{"a":[2,1]}`)
 	checkNotEqual(t, `{"a":"b"}`, `{"a":"c"}`)
