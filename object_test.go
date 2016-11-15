05178e0c12ebed50ca1c6bccaebf4b601fbadc64
Joseph Burnett <josephburnett79@gmail.com>
2016-11-14 20:22:48 -0800

    More Object tests.

--- object_test.go
+++ object_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
+func TestObjectJson(t *testing.T) {
+	checkJson(t, `{"a":1}`, `{"a":1}`)
+	checkJson(t, ` { "a" : 1 } `, `{"a":1}`)
+	checkJson(t, `{}`, `{}`)
+}
+
 func TestObjectEqual(t *testing.T) {
 	checkEqual(t, `{"a":1}`, `{"a":1}`)
 	checkEqual(t, `{"a":1}`, `{"a":1.0}`)
@@ -15,4 +21,27 @@ func TestObjectNotEqual(t *testing.T) {
 	checkNotEqual(t, `{"a":1}`, `{"b":1}`)
 	checkNotEqual(t, `{"a":[1,2]}`, `{"a":[2,1]}`)
 	checkNotEqual(t, `{"a":"b"}`, `{"a":"c"}`)
+}
+
+func TestObjectDiff(t *testing.T) {
+	checkDiff(t, `{}`, `{}`)
+	checkDiff(t, `{"a":1}`, `{"a":1}`)
+	checkDiff(t, `{"a":1}`, `{"a":2}`,
+		`@ ["a"]`,
+		`- 1`,
+		`+ 2`)
+	checkDiff(t, `{"":1}`, `{"":1}`)
+	checkDiff(t, `{"":1}`, `{"a":2}`,
+		`@ [""]`,
+		`- 1`,
+		`@ ["a"]`,
+		`+ 2`)
+}
+
+func testObjectPatch(t *testing.T) {
+
+}
+
+func testObjectPatchError(t *testing.T) {
+
 }
