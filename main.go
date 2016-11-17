6bf801ad53a4264e5d2e993d93dcc8e480182c92
Joseph Burnett <josephburnett79@gmail.com>
2016-11-17 00:05:16 -0800

    Better usage.

--- main.go
+++ main.go
@@ -35,12 +35,22 @@ func main() {
 
 func printUsageAndExit() {
 	for _, line := range []string{
-		`Usage: jd <json_file_a> <json_file_b>`,
-		`       jd -p <patch_file> <json_file_a>`,
-		`       jd <json_file_a>`,
-		`       jd -p <patch_file>`,
+		``,
+		`Usage: jd [OPTION]... FILE1 [FILE2]`,
+		`Diff and patch JSON files.`,
+		``,
+		`When FILE2 is omitted the second input is read from STDIN.`,
+		``,
+		`Options:`,
+		`  -p  Apply patch FILE1 to FILE2 or STDIN.`,
+		``,
+		`Examples:`,
+		`  jd a.json b.json`,
+		`  cat b.json | jd a.json`,
+		`  jd a.json b.json > patch; jd patch a.json`,
+		``,
 	} {
-		fmt.Print(line)
+		fmt.Println(line)
 	}
 	os.Exit(1)
 }
