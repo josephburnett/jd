0eeba922bcf0bdbd109b77fc9d9a883415baa409
Joseph Burnett <josephburnett79@gmail.com>
2022-02-08 16:09:11 +0100

    Check round trip fuzzing.

--- lib/fuzz_test.go
+++ lib/fuzz_test.go
@@ -17,7 +17,7 @@ func FuzzJd(f *testing.F) {
 			return
 		}
 		if a == nil {
-			t.Errorf("nil parsed input: %v", aStr)
+			t.Errorf("nil parsed input: %q", aStr)
 			return
 		}
 		b, err := ReadJsonString(bStr)
@@ -25,12 +25,29 @@ func FuzzJd(f *testing.F) {
 			return
 		}
 		if b == nil {
-			t.Errorf("nil parsed input: %v", bStr)
+			t.Errorf("nil parsed input: %q", bStr)
 			return
 		}
-		diffAB := a.Diff(b)
-		if diffAB == nil {
+		// Diff A and B.
+		d := a.Diff(b)
+		if d == nil {
 			t.Errorf("nil diff of a and b")
+			return
+		}
+		diffABStr := d.Render()
+		diffAB, err := ReadDiffString(diffABStr)
+		if err != nil {
+			t.Errorf("error parsing diff string %q: %v", diffABStr, err)
+			return
+		}
+		// Apply diff to A to get B.
+		patchedA, err := a.Patch(diffAB)
+		if err != nil {
+			t.Errorf("applying patch %v to %v should give %v. Got err: %v", diffAB, aStr, bStr, err)
+			return
+		}
+		if !patchedA.Equals(b) {
+			t.Errorf("applying patch %v to %v should give %v. Got err: %v", diffAB, aStr, bStr, patchedA)
 			return
 		}
 	})
