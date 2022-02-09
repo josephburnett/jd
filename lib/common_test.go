bd2fc2657f56c7c1f70a5971c18be32365afd1c7
Joseph Burnett <josephburnett79@gmail.com>
2022-02-09 18:46:40 +0100

    Check for void nodes on JSON Patch render.

--- lib/common_test.go
+++ lib/common_test.go
@@ -242,11 +242,11 @@ func fuzz(t *testing.T, aStr, bStr string) {
 		// Apply diff to A to get B.
 		patchedA, err := a.Patch(diffAB)
 		if err != nil {
-			t.Errorf("applying patch %v to %v should give %v. Got err: %v", diffAB, aStr, bStr, err)
+			t.Errorf("applying patch %v to %v should give %v. Got err: %v", diffAB.Render(), aStr, bStr, err)
 			return
 		}
 		if !patchedA.Equals(b) {
-			t.Errorf("applying patch %v to %v should give %v. Got err: %v", diffAB, aStr, bStr, patchedA)
+			t.Errorf("applying patch %v to %v should give %v. Got: %v", diffAB.Render(), aStr, bStr, patchedA)
 			return
 		}
 	}
--- lib/fuzz_backport_test.go
+++ lib/fuzz_backport_test.go
@@ -6,10 +6,12 @@ import "testing"
 // versions before 1.18. This is only necessary until go1.18beta2 is GA
 // and we can depend on it.
 //
-// To run fuzzing use `go1.18beta2 test -tags=test_fuzz ./lib/-run=FuzzJd`
+// To run fuzzing use `go1.18beta2 test -tags=test_fuzz ./lib/ -fuzz=FuzzJd`
 func TestFuzzBackport(t *testing.T) {
 	for _, backport := range [][2]string{{
-		"[]", "0",
+		"[]", "0", // FuzzJd/e193f6c4bfd5b8d3c12e1ac42162b2ccd7a31f9aafd466066c1ec7a95da48e1e
+	}, {
+		"{}", " ", // FuzzJd/868060b2021521d32933f40415c6f95b38fda5f5c6bdb7fa6664d046c637c03c
 	}} {
 		fuzz(t, backport[0], backport[1])
 	}
