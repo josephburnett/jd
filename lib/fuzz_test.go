64f59d422d1839d2bfa3884eac7b143fdedf082d
Joseph Burnett <josephburnett79@gmail.com>
2022-02-09 21:00:59 +0100

    Expand baseline corpus.

--- lib/fuzz_test.go
+++ lib/fuzz_test.go
@@ -6,9 +6,49 @@ import (
 	"testing"
 )
 
+var corpus = []string{
+	``,  // void
+	` `, // void
+	`null`,
+	`0`,
+	`1`,
+	`""`,
+	`"foo"`,
+	`"bar"`,
+	`"null"`,
+	`[]`,
+	`[null]`,
+	`[null,null,null]`,
+	`[1]`,
+	`[1,2,3]`,
+	`[{},[],3]`,
+	`[1,{},[]]`,
+	`{}`,
+	`{"foo":"bar"}`,
+	`{"foo":null}`,
+	`{"foo":1}`,
+	`{"foo":[]}`,
+	`{"foo":[null]}`,
+	`{"foo":[1]}`,
+	`{"foo":[1,2,3]}`,
+	`{"foo":[1,null,3]}`,
+	`{"foo":{}}`,
+	`{"foo":{"bar":null}}`,
+	`{"foo":{"bar":1}}`,
+	`{"foo":{"bar":[]}}`,
+	`{"foo":{"bar":[1,2,3]}}`,
+	`{"foo":{"bar":{}}}`,
+}
+
 func FuzzJd(f *testing.F) {
-	a := `{"foo":{},"bar":[1,null,3]}`
-	b := a
-	f.Add(a, b)
+	for _, a := range corpus {
+		_, err := ReadJsonString(a)
+		if err != nil {
+			f.Errorf("corpus entry not valid JSON: %q", a)
+		}
+		for _, b := range corpus {
+			f.Add(a, b)
+		}
+	}
 	f.Fuzz(fuzz)
 }
