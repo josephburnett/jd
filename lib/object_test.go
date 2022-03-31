14f8e513260335c505c9fea84ea479798fbacc84
Joseph Burnett <josephburnett79@gmail.com>
2022-03-31 21:43:31 +0200

    Convert object tests into table tests.

--- lib/object_test.go
+++ lib/object_test.go
@@ -6,99 +6,240 @@ import (
 
 func TestObjectJson(t *testing.T) {
 	ctx := newTestContext(t)
-	checkJson(ctx, `{"a":1}`, `{"a":1}`)
-	checkJson(ctx, ` { "a" : 1 } `, `{"a":1}`)
-	checkJson(ctx, `{}`, `{}`)
+	tests := []struct{
+		a string
+		b string
+	}{{
+		a:`{"a":1}`,
+		b: `{"a":1}`,
+	}, {
+		a: ` { "a" : 1 } `,
+		b:`{"a":1}`,
+	}, {
+		a:`{}`,
+		b:`{}`,
+	}}
+
+	for _, tt := range tests {
+		checkJson(ctx, tt.a, tt.b)
+	}
 }
 
 func TestObjectEqual(t *testing.T) {
 	ctx := newTestContext(t)
-	checkEqual(ctx, `{"a":1}`, `{"a":1}`)
-	checkEqual(ctx, `{"a":1}`, `{"a":1.0}`)
-	checkEqual(ctx, `{"a":[1,2]}`, `{"a":[1,2]}`)
-	checkEqual(ctx, `{"a":"b"}`, `{"a":"b"}`)
+	tests := []struct{
+		a string
+		b string
+	}{{
+		a:`{"a":1}`,
+		b:`{"a":1}`,
+	}, {
+		a: `{"a":1}`,
+		b:`{"a":1.0}`,
+	}, {
+		a: `{"a":[1,2]}`,
+		b: `{"a":[1,2]}`,
+	}, {
+		a: `{"a":"b"}`,
+		b: `{"a":"b"}`,
+	}}
+
+	for _, tt := range tests {
+		checkEqual(ctx, tt.a, tt.b)
+	}
 }
 
 func TestObjectNotEqual(t *testing.T) {
 	ctx := newTestContext(t)
-	checkNotEqual(ctx, `{"a":1}`, `{"b":1}`)
-	checkNotEqual(ctx, `{"a":[1,2]}`, `{"a":[2,1]}`)
-	checkNotEqual(ctx, `{"a":"b"}`, `{"a":"c"}`)
+	tests := []struct{
+		a string
+		b string
+	}{{
+		a: `{"a":1}`,
+		b: `{"b":1}`,
+	}, {
+		a: `{"a":[1,2]}`,
+		b:`{"a":[2,1]}`,
+	}, {
+		a: `{"a":"b"}`,
+		b:`{"a":"c"}`,
+	}}
+
+	for _, tt := range tests {
+		checkNotEqual(ctx, tt.a, tt.b)
+	}
 }
 
 // TODO: add unit test for object identity with setkeys metadata.
 func TestObjectHash(t *testing.T) {
 	ctx := newTestContext(t)
-	checkHash(ctx, `{}`, `{}`, true)
-	checkHash(ctx, `{"a":1}`, `{"a":1}`, true)
-	checkHash(ctx, `{"a":1}`, `{"a":2}`, false)
-	checkHash(ctx, `{"a":1}`, `{"b":1}`, false)
-	checkHash(ctx, `{"a":1,"b":2}`, `{"b":2,"a":1}`, true)
+	tests := []struct{
+		a string
+		b string
+		wantEqual bool
+	}{{
+		a: `{}`,
+		b:`{}`,
+		wantEqual: true,
+	}, {
+		a: `{"a":1}`,
+		b:`{"a":1}`,
+		wantEqual: true,
+	}, {
+		a: `{"a":1}`,
+		b:`{"a":2}`,
+		wantEqual: false,
+	}, {
+		a: `{"a":1}`,
+		b:`{"b":1}`,
+		wantEqual: false,
+	}, {
+		a: `{"a":1,"b":2}`,
+		b: `{"b":2,"a":1}`,
+		wantEqual: true,
+	}}
+
+	for _, tt := range tests {
+		checkHash(ctx, tt.a, tt.b, tt.wantEqual)
+	}
 }
 
 func TestObjectDiff(t *testing.T) {
 	ctx := newTestContext(t)
-	checkDiff(ctx, `{}`, `{}`)
-	checkDiff(ctx, `{"a":1}`, `{"a":1}`)
-	checkDiff(ctx, `{"a":1}`, `{"a":2}`,
-		`@ ["a"]`,
-		`- 1`,
-		`+ 2`)
-	checkDiff(ctx, `{"":1}`, `{"":1}`)
-	checkDiff(ctx, `{"":1}`, `{"a":2}`,
-		`@ [""]`,
-		`- 1`,
-		`@ ["a"]`,
-		`+ 2`)
-	checkDiff(ctx, `{"a":{"b":{}}}`, `{"a":{"b":{"c":1},"d":2}}`,
-		`@ ["a","b","c"]`,
-		`+ 1`,
-		`@ ["a","d"]`,
-		`+ 2`)
-	// regression test for issue #18
-	checkDiff(ctx,
-		`{"R": [{"I": [{"T": [{"V": "t","K": "N"},{"V": "T","K": "I"}]}]}]}`,
-		`{"R": [{"I": [{"T": [{"V": "t","K": "N"},{"V": "Q","K": "C"},{"V": "T","K": "I"}]}]}]}`,
-		`@ ["R",0,"I",0,"T",1,"K"]`,
-		`- "I"`,
-		`+ "C"`,
-		`@ ["R",0,"I",0,"T",1,"V"]`,
-		`- "T"`,
-		`+ "Q"`,
-		`@ ["R",0,"I",0,"T",-1]`,
-		`+ {"K":"I","V":"T"}`)
+	tests := []struct{
+		a string
+		b string
+		diff []string
+	}{{
+		a: `{}`,
+		b: `{}`,
+	}, {
+		a: `{"a":1}`,
+		b: `{"a":1}`,
+	}, {
+		a: `{"a":1}`,
+		b: `{"a":2}`,
+		diff: ss(
+			`@ ["a"]`,
+			`- 1`,
+			`+ 2`,
+		),
+	}, {
+		a: `{"":1}`,
+		b: `{"":1}`,
+	}, {
+		a: `{"":1}`,
+		b: `{"a":2}`,
+		diff: ss(
+			`@ [""]`,
+			`- 1`,
+			`@ ["a"]`,
+			`+ 2`,
+		),
+	}, {
+		a: `{"a":{"b":{}}}`,
+		b: `{"a":{"b":{"c":1},"d":2}}`,
+		diff: ss(
+			`@ ["a","b","c"]`,
+			`+ 1`,
+			`@ ["a","d"]`,
+			`+ 2`,
+		),
+	}, {
+		// regression test for issue #18
+		a: `{"R": [{"I": [{"T": [{"V": "t","K": "N"},{"V": "T","K": "I"}]}]}]}`,
+		b: `{"R": [{"I": [{"T": [{"V": "t","K": "N"},{"V": "Q","K": "C"},{"V": "T","K": "I"}]}]}]}`,
+		diff: ss(
+			`@ ["R",0,"I",0,"T",1,"K"]`,
+			`- "I"`,
+			`+ "C"`,
+			`@ ["R",0,"I",0,"T",1,"V"]`,
+			`- "T"`,
+			`+ "Q"`,
+			`@ ["R",0,"I",0,"T",-1]`,
+			`+ {"K":"I","V":"T"}`,
+		),
+	}}
+
+	for _, tt := range tests {
+		checkDiff(ctx, tt.a, tt.b, tt.diff...)
+	}
 }
 
 func testObjectPatch(t *testing.T) {
 	ctx := newTestContext(t)
-	checkPatch(ctx, `{}`, `{}`)
-	checkPatch(ctx, `{"a":1}`, `{"a":1}`)
-	checkPatch(ctx, `{"a":1}`, `{"a":2}`,
-		`@ ["a"]`,
-		`- 1`,
-		`+ 2`)
-	checkPatch(ctx, `{"":1}`, `{"":1}`)
-	checkPatch(ctx, `{"":1}`, `{"a":2}`,
-		`@ [""]`,
-		`- 1`,
-		`@ ["a"]`,
-		`+ 2`)
-	checkPatch(ctx, `{"a":{"b":{}}}`, `{"a":{"b":{"c":1},"d":2}}`,
-		`@ ["a","b","c"]`,
-		`+ 1`,
-		`@ ["a","d"]`,
-		`+ 2`)
+	tests := []struct{
+		a string
+		b string
+		diff []string
+	}{{
+		a: `{}`,
+		b: `{}`,
+	}, {
+		a: `{"a":1}`,
+		b: `{"a":1}`,
+	}, {
+		a: `{"a":1}`,
+		b: `{"a":2}`,
+		diff: ss(
+			`@ ["a"]`,
+			`- 1`,
+			`+ 2`,
+		),
+	}, {
+		a: `{"":1}`,
+		b: `{"":1}`,
+	}, {
+		a: `{"":1}`,
+		b: `{"a":2}`,
+		diff: ss(
+			`@ [""]`,
+			`- 1`,
+			`@ ["a"]`,
+			`+ 2`,
+		),
+	}, {
+		a: `{"a":{"b":{}}}`,
+		b: `{"a":{"b":{"c":1},"d":2}}`,
+		diff: ss(
+			`@ ["a","b","c"]`,
+			`+ 1`,
+			`@ ["a","d"]`,
+			`+ 2`,
+		),
+	}}
+
+	for _, tt := range tests {
+		checkPatch(ctx, tt.a, tt.b, tt.diff...)
+	}
 }
 
 func testObjectPatchError(t *testing.T) {
 	ctx := newTestContext(t)
-	checkPatchError(ctx, `{}`,
-		`@ ["a"]`,
-		`- 1`)
-	checkPatchError(ctx, `{"a":1}`,
-		`@ ["a"]`,
-		`+ 2`)
-	checkPatchError(ctx, `{"a":1}`,
-		`@ ["a"]`,
-		`+ 1`)
+	tests := []struct{
+		a string
+		diff []string
+	}{{
+		a: `{}`,
+		diff: ss(
+			`@ ["a"]`,
+			`- 1`,
+		),
+	}, {
+		a: `{"a":1}`,
+		diff: ss(
+			`@ ["a"]`,
+			`+ 2`,
+		),
+	}, {
+		a: `{"a":1}`,
+		diff: ss(
+			`@ ["a"]`,
+			`+ 1`,
+		),
+	}}
+
+	for _, tt := range tests {
+		checkPatchError(ctx, tt.a, tt.diff...)
+	}
 }
