200ee90d114bae90bd7fd515eeaed6e5599dce69
Joseph Burnett <josephburnett79@gmail.com>
2022-06-17 23:10:08 +0200

    Revert "Fix read void merge patch."
    This reverts commit 187cf56a3864bbb512c3017738ca91e24f617bee.

--- lib/diff_read.go
+++ lib/diff_read.go
@@ -224,9 +224,6 @@ func ReadMergeString(s string) (Diff, error) {
 		return nil, err
 	}
 	d := Diff{}
-	if isVoid(n) {
-		return d, nil
-	}
 	p := []JsonNode{jsonArray{jsonString(MERGE.string())}}
 	return readMergeInto(d, p, n), nil
 }
