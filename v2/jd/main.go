c4d389eb70a959dea226c3e0b77ff60ab4ac642f
Joseph Burnett <josephburnett79@gmail.com>
2025-12-27 20:26:39 -0800

    Set version 2.3.1

--- v2/jd/main.go
+++ v2/jd/main.go
@@ -18,7 +18,7 @@ import (
 	"github.com/josephburnett/jd/v2/web/serve"
 )
 
-const version = "2.3.0"
+const version = "2.3.1"
 
 var (
 	color         = flag.Bool("color", false, "Print color diff")
